81331df376299c573d71b5c014ef32e86a76214c
Warwick Stone <warwick_stone@fastmail.com>
2023-10-07 14:51:40 +1100

    cs runs in docker

--- src/cs/run.sh
+++ src/cs/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-DATA=../../data/tsp_85900_1
-docker run --rm -v $DATA:/data langshowdown-cs /data
+ROOT=$(realpath ../..)
+DATA=$ROOT/data
+docker run --rm -v $DATA:/App/data langshowdown-cs data/tsp/tsp_85900_1
